72f428941f91f57c50ce10f484b074a32ef98e13
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-26 10:51:07 -0500

    Update review_blame.py

--- actions/review_blame.py
+++ actions/review_blame.py
@@ -1,5 +1,14 @@
+import os
 from git import Repo
 
-repo = Repo(self.rorepo.working_tree_dir)
-assert not repo.bare
-print(repo)
+repo_path = os.getenv('GIT_REPO_PATH')
+
+repo = Repo(repo_path)
+assert repo.bare
+
+print('Repo at {} successfully loaded.'.format(repo_path))
+print('Repo description: {}'.format(repo.description))
+print('Repo active branch is {}'.format(repo.active_branch))
+for remote in repo.remotes:
+        print('Remote named "{}" with URL "{}"'.format(remote, remote.url))
+print('Last commit for repo is {}.'.format(str(repo.head.commit.hexsha)))
