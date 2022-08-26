dff3738c777384bd65d63db5c30c6f82206646cf
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-25 20:46:21 -0500

    Update verify.py

--- actions/verify.py
+++ actions/verify.py
@@ -1,3 +1,29 @@
 import tomli
+import os
+import sys
+from urllib.parse import urlparse
 
-raise Exception("test")
+with open("public/refs.toml", "rb") as f:
+    toml = tomli.load(f)
+authorcount = {}
+for w, x in toml.items():
+    if type(x) != dict:
+        raise SyntaxError(f"Toml Data invalid: {x}")
+    for y, z in x.items():
+        if type(x) != dict:
+            raise SyntaxError(f"Toml Data invalid: {x}")
+        if type(z["val"]) != str:
+            raise TypeError("Values can only be strings.")
+        if type(z["author"]) != str:
+            raise TypeError("Values can only be strings.")
+        #if z["author"] != os.getenv("author"):
+            #raise ValueError(f"Wrong author: {z['author']}, should be {os.getenv('author')}")
+        url = urlparse(z["val"])
+        if not url.hostname.split(".")[-2:] in [["github", "com"], ["github", "io"]]:
+            raise ValueError("URL is not a valid github url.")
+        if authorcount.get(z["author"]):
+            authorcount[z["author"]] += 1
+        else:
+            authorcount[z["author"]] = 1
+        if authorcount[z["author"]] > 5:
+            sys.exit("To many url's by author.")
