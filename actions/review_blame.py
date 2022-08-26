07e143d4dac714cec9cd466851ab3dd6105bbde1
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-26 11:37:46 -0500

    Update review_blame.py

--- actions/review_blame.py
+++ actions/review_blame.py
@@ -1,14 +1,1 @@
-import github
-import asyncio
 
-
-async def main():
-    client = await github.GHClient()
-
-    user = await client.get_user(user="Github Actions")
-
-    print(user)
-    print(user.html_url)
-
-
-asyncio.run(main())
