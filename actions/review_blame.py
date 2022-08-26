4a30dc898aba2dc459bcc31c6e6afa5856c6489a
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-26 12:27:43 -0500

    Update review_blame.py

--- actions/review_blame.py
+++ actions/review_blame.py
@@ -1,4 +1,10 @@
 import os
 
-diff = os.getenv("DIFF")
+def getitems(l):
+    l = l.split(" ")
+    for x in l:
+        l[l.index(x)] = eval(x)
+    return l
+
+diff = getitems(os.getenv("DIFF"))
 print(diff)
